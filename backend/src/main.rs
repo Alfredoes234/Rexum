2d07ae578cd42618a6edf3d4311e6262041d1644
Alfredoes234 <alfie.rayner30@gmail.com>
2024-01-11 12:03:32 +0000

    Update backend dependencies and add new routes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,23 +1,20 @@
-use anyhow::Result;
+use anyhow::{Error, Result};
 use axum::{
-    extract::{Path, Query},
+    debug_handler,
+    extract::{Extension, Path, Query, FromRef, FromRequestParts},
+    http::StatusCode,
     middleware,
-    response::{Html, IntoResponse, Response},
+    response::{Html,  Response},
     routing::{get, post},
-    Json, Router, http::StatusCode,
+    Json, Router,
 };
 use serde::Deserialize;
 use serde_json::{json, Value};
 use sqlx::mysql::MySqlPoolOptions;
-use sqlx::{query, MySqlPool};
+use sqlx::MySqlPool;
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    // Database connection
-    let pool = MySqlPoolOptions::new()
-        .max_connections(5)
-        .connect("mysql://root:root@localhost/main")
-        .await?;
     // build our application with a route
     let app = Router::new()
         .merge(routes())
@@ -30,35 +27,29 @@ async fn main() -> Result<(), sqlx::Error> {
     axum::serve(listener, app).await.unwrap();
 
     // Queries
-
     Ok(())
+}
+
+async fn pool() -> MySqlPool {
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect("mysql://root:root@localhost/main")
+        .await
+        .unwrap();
+    return pool;
 }
 
 async fn main_response_mapper(res: Response) -> Response {
     println!("{:<12} - main_response_mapper", "RES_MAPPER");
+    println!("{res:?}");
     res
 }
 
 fn routes() -> Router {
     Router::new()
         .route("/", get(index))
-        .route("/api/json", get(json))
-        .route("/api/hello", get(hello))
-        .route("/api/hello2/:name", get(hello2))
         .route("/api/user_a", post(add_user))
 }
-
-struct AppError(anyhow::Error);
-impl IntoResponse for AppError {
-    fn into_response(self) -> Response {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", self.0),
-        )
-            .into_response()
-    }
-}
-
 
 #[derive(serde::Serialize, serde::Deserialize)]
 struct UserBody<T> {
@@ -69,7 +60,7 @@ struct UserBody<T> {
 struct User {
     name: Option<String>,
     email: String,
-    pasword: String,
+    password: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -79,55 +70,36 @@ struct NewUser {
     password: String,
 }
 
-async fn add_user(pool: &MySqlPool, Query(params): Query<NewUser>) -> Json<UserBody<User>> {
-    let user = query!(
-        r#"
+async fn add_user(
+    Json(params): Json<NewUser>,
+) -> Json<UserBody<User>> {
+    let pool = pool().await;
+    println!("{params:?}");
+    let result = sqlx::query(
+        "
         INSERT INTO Users(name, email, password)
         VALUES (?, ?, ?);
-        "#,
-        params.name,
-        params.email,
-        params.password
+        ",
     )
-    .execute(pool)
-    .await
-    .unwrap();
-    Json(UserBody {
-        user: User {
-            name: params.name,
-            email: params.email,
-            pasword: params.password,
-        },
-    })
+    .bind(&params.name)
+    .bind(&params.email)
+    .bind(&params.password)
+    .execute(&pool)
+    .await;
+
+    match result {
+        Ok(_) => Json(UserBody {
+            user: User {
+                name: params.name,
+                email: params.email,
+                password: params.password,
+            },
+        }),
+        Err(_) => panic!(),
+    }
 }
 
 async fn index() -> Html<&'static str> {
     println!("{:<12} - main_Site", "HANDLER");
     Html("<p>Pirate</p>")
-}
-
-async fn json() -> Json<Value> {
-    println!("{:<12} - json", "HANDLER");
-    Json(json!({
-        "id": 0,
-        "data": 42
-    }))
-}
-
-#[derive(Debug, Deserialize)]
-struct HelloParams {
-    name: Option<String>,
-}
-
-async fn hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
-    println!("{:<12} - handler_hello - {params:?}", "HANDLER");
-
-    let name = params.name.as_deref().unwrap_or("World!");
-    Html(format!("Hello <strong>{name}<strong"))
-}
-
-async fn hello2(Path(name): Path<String>) -> impl IntoResponse {
-    println!("{:<12} - handler_hello - {name:?}", "HANDLER");
-
-    Html(format!("Hello <strong>{name}<strong"))
 }
